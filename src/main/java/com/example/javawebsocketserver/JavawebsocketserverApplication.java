57102901779d32a33ff9546ef6ca80f02fc8bf91
anshdevops <anshdevops@users.noreply.github.com>
2020-05-29 11:11:36 +0530

    first commit

---
+++ src/main/java/com/example/javawebsocketserver/JavawebsocketserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebsocketserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavawebsocketserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javawebsocketserver/JavawebsocketserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebsocketserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
